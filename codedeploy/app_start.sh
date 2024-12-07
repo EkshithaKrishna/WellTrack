29be3d6d8d96e2d5942da9e1f84412ddf03400b2
EkshithaKrishna <EkshithaKrishna11806799@gmail.com>
2024-12-07 11:16:24 +0530

    Update app_start.sh

--- codedeploy/app_start.sh
+++ codedeploy/app_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-pkill gunicorn
 cd /home/ubuntu/se-project/WellTrack/
+python3 -m venv venv
+source venv/bin/activate
+pkill gunicorn
 gunicorn WellTrack.wsgi:application --bind 0.0.0.0:8000 --daemon
